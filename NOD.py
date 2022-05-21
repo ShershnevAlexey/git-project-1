e42d7a72a027a7c1aae7d25ba41e73544634ecf6
Alexey Shershnev <shershnev35@gmail.com>
2022-05-22 00:49:10 +0300

    second commit

---
+++ NOD.py
@@ -0,0 +1,9 @@
+a = int(input('enter first number\n'))
+b = int(input('enter second number\n'))
+while a != 0 and b != 0:
+    print('One more  step')
+    if a > b:
+        a = a % b
+    else:
+        b = b % a
+print(a + b)
