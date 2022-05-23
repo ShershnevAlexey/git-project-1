925130e5a613ba785c93611baa9b2aed0fc7f96d
Alexey Shershnev <shershnev35@gmail.com>
2022-05-23 22:15:00 +0300

    add comment

--- multic.py
+++ multic.py
@@ -1,4 +1,5 @@
+# multiplication table
 for i in range(1, 10):
     for j in range(1, 10):
-        print(j, '*', i, '=', j *i, end = '!!\t')
+        print(j, '*', i, '=', j *i, end = '\t')
     print('\n')	
